dbc080a7b350735c54ceedc815c3b7db40058aa1
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-06 22:51:27 +0300

    brand, category and product serializers

--- stock/serializers.py
+++ stock/serializers.py
@@ -9,3 +9,40 @@ class CategorySerializer(serializers.ModelSerializer):
             'id',
             'name'
         )
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Brand
+        fields=(
+            'id',
+            'name'
+        )
+
+class ProductSerializer(serializers.ModelSerializer):
+    category=serializers.StringRelatedField()
+    category_id=serializers.IntegerField(write_only=True)
+    brand=serializers.StringRelatedField()
+    brand_id=serializers.IntegerField(write_only=True)
+    class Meta:
+        model=Product
+        fields=(
+            'id',
+            'name',
+            'category',
+            'category_id',
+            'brand',
+            'brand_id',
+            'stock',
+        )
+        read_only_fields = ('stock',)
+
+
+class FirmSerializer(serializers.ModelSerializer):
+    model=Firm
+    fields=(
+            'id',
+            'name',
+            'phone',
+            'address',
+    )
