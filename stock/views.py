11ce47cfa61cde0f92e0edffdfe153c47ca1f2c8
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-06 22:51:50 +0300

    related views updated

--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,37 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import(Category, Brand, Product, Firm, Transaction)
+
+from .serializers import (
+    CategorySerializer, BrandSerializer, ProductSerializer,FirmSerializer
+)
 
 
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = ''
+    serializer_class = CategorySerializer
+    filter_backends=[filters.SearchFilter]
+    search_fields=['name']
+
+
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    filter_backends=[filters.SearchFilter]
+    search_fields=['name']
+
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields=['category', 'brand']
+    search_fields=['name']
+
+class FirmView(viewsets.ModelViewSet):
+    queryset = Firm.objects.all()
+    serializer_class = FirmSerializer
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter]
+    
+    search_fields=['name']
