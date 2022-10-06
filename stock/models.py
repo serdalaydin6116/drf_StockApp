ab47c0dfba6ed1f12816c21baa55760929747274
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-06 21:10:24 +0300

    category and brand models were created

--- stock/models.py
+++ stock/models.py
@@ -8,6 +8,23 @@ class UpdateCreate(models.Model):
 
     class Meta:
         abstract=True
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Brand(models.Model):
+    name=models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.name
+
+
     
 
 
