104bbc16ee423f89b75fec63ab9cd8b81245e698
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-06 22:11:22 +0300

    updated

--- stock/models.py
+++ stock/models.py
@@ -38,7 +38,7 @@ class Product(UpdateCreate):
 class Firm(UpdateCreate):
     name=models.CharField(max_length=25)
     phone=models.CharField(max_length=15)
-    address=models.CharField(max_length=200)  ##textfield gereksiz uzun yer kaplar..
+    address=models.CharField(max_length=200)  ##using textfield results in unnecessary DB storage using.
 
     def __str__(self):
         return self.name
@@ -52,12 +52,12 @@ class Transaction(UpdateCreate):
     )
 
     user=models.ForeignKey(User,on_delete=models.SET_NULL, null=True )
-    firm=models.ForeignKey(Firm, on_delete=SET_NULL, null=True, related_name='transactions')
+    firm=models.ForeignKey(Firm, on_delete=models.SET_NULL, null=True, related_name='transactions')
     transaction=models.SmallIntegerField(choices=TRANSACTION)
     product=models.ForeignKey(Product,on_delete=models.CASCADE, related_name='transaction')
     quantity=models.SmallIntegerField()
-    price=models.DecimalField(max_digit=6, decimal_places=2)
-    price_total=models.DecimalField(max_digit=8, decimal_places=2)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+    price_total=models.DecimalField(max_digits=8, decimal_places=2, blank=True)
 
     def __str__(self):
         return f'{self.transaction} - {self.product} - {self.quantity}'
