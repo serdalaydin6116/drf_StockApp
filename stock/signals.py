ee20ec36fd708f42e7428b5bb271ab38469b17e7
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-07 14:07:41 +0300

    imports were designed

--- stock/signals.py
+++ stock/signals.py
@@ -2,21 +2,22 @@ from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .models import Transaction, Product
 
-@receiver(pre_save, sender=Transaction)  # ne xaman yapacak pre_save de, kimi dinleyecek= transaction modelini
+
+@receiver(pre_save, sender=Transaction)
 def calculate_total_price(sender, instance, **kwargs):
     if not instance.price_total:
-        instance.price_total=instance.quantity * instance.price
+        instance.price_total = instance.quantity * instance.price
 
 
 @receiver(post_save, sender=Transaction)
 def update_stock(sender, instance, **kwargs):
-    product=Product.objects.get(id=instance.product_id)
-    if instance.transaction==1:
+    product = Product.objects.get(id=instance.product_id)
+    if instance.transaction == 1:
         if not product.stock:
-            product.stock=instance.quantity
+            product.stock = instance.quantity
         else:
             product.stock += instance.quantity
-
     else:
-        product.stock -=instance.quantity
+        product.stock -= instance.quantity
+
     product.save()
